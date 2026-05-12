4ce1fa31ada9930ff9ec146c0e7e0776569ba946
Naveen-3311 <naveen@hyniva.com>
2026-05-12 12:57:35 +0530

    EL-810 : Implemented common summery cards

---
+++ src/common/CommonSummaryCardGroup.jsx
@@ -0,0 +1,36 @@
+import { Box } from "@mui/material";
+import CommonSummaryCard from "./CommonSummaryCard";
+
+const CommonSummaryCardGroup = ({ cards = [], showAccentBar = true }) => {
+  if (!cards.length) return null;
+
+  return (
+    <Box
+      mb={2}
+      sx={{
+        display: "grid",
+        gridTemplateColumns: {
+          xs: "repeat(1, 1fr)",
+          sm: "repeat(2, 1fr)",
+          md: "repeat(2, 1fr)",
+          lg: "repeat(2, 1fr)",
+          xl: "repeat(auto-fit, minmax(0, 1fr))",
+        },
+        gap: 2,
+      }}
+    >
+      {cards.map((card) => (
+        <CommonSummaryCard
+          key={card.id}
+          title={card.title}
+          value={card.value}
+          accentcolor={card.accentcolor}
+          icon={card.icon}
+          showAccentBar={showAccentBar}
+        />
+      ))}
+    </Box>
+  );
+};
+
+export default CommonSummaryCardGroup;
--- src/components/AdminComponents/Dummy.jsx
+++ src/components/AdminComponents/Dummy.jsx
@@ -1,5 +1,79 @@
+import CommonSummaryCardGroup from "../../common/CommonSummaryCardGroup";
+import { PageContainer } from "./component.styled";
+
+
+import DevicesIcon from "@mui/icons-material/Devices";
+import WifiIcon from "@mui/icons-material/Wifi";
+import WifiOffIcon from "@mui/icons-material/WifiOff";
+import Inventory2OutlinedIcon from "@mui/icons-material/Inventory2Outlined";
+
+const summaryCards = [
+  {
+    id: "total_devices",
+    title: "Total Devices",
+    value: "1,095",
+    accentcolor: "#284495",
+    icon: (
+      <DevicesIcon
+        sx={{
+          fontSize: 28,
+          color: "#284495",
+        }}
+      />
+    ),
+  },
+  {
+    id: "online_devices",
+    title: "Online Devices",
+    value: "1,077",
+    accentcolor: "#008236",
+    icon: (
+      <WifiIcon
+        sx={{
+          fontSize: 28,
+          color: "#008236",
+        }}
+      />
+    ),
+  },
+  {
+    id: "offline_devices",
+    title: "Offline Devices",
+    value: "18",
+    accentcolor: "#FF0000",
+    icon: (
+      <WifiOffIcon
+        sx={{
+          fontSize: 28,
+          color: "#FF0000",
+        }}
+      />
+    ),
+  },
+  {
+    id: "unassigned_devices",
+    title: "Unassigned Devices",
+    value: "34",
+    accentcolor: "#E69500",
+    icon: (
+      <Inventory2OutlinedIcon
+        sx={{
+          fontSize: 28,
+          color: "#E69500",
+        }}
+      />
+    ),
+  },
+];
+
 const Dummy = () => {
-      return <>Hi!!! </>;
+      return (
+        <>
+        <PageContainer>
+          <CommonSummaryCardGroup cards={summaryCards} showAccentBar={false} />
+          </PageContainer>
+        </>
+      );
 }
 
 export default Dummy;
