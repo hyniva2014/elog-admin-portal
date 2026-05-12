ed439064afa1de8243bc3bde9306de246fe1a666
harish-hyniva <harish.chaluvadi@hyniva.com>
2026-05-12 11:14:51 +0530

    EL-803 : Add side menus under comliance

--- src/common/menu-items.js
+++ src/common/menu-items.js
@@ -11,6 +11,36 @@ const MENU_ITEMS = [
         url: "/dashboard",
         parentKey: "compliance",
       },
+      {
+        key: "device-asset-management",
+        label: "Device Asset Management",
+        url: "/device-asset-management",
+        parentKey: "compliance",
+      },
+      {
+        key: "account-management",
+        label: "Account Management",
+        url: "/account-management",
+        parentKey: "compliance",
+      },
+      {
+        key: "device-model-management",
+        label: "Device Model Management",
+        url: "/device-model-management",
+        parentKey: "compliance",
+      },
+      {
+        key: "device-management",
+        label: "Device Management",
+        url: "/device-management",
+        parentKey: "compliance",
+      },
+      {
+        key: "user-management",
+        label: "User Management",
+        url: "/user-management",
+        parentKey: "compliance",
+      },
     ],
   },
 ];
---
+++ src/components/compliance/UserManagement/UserManagement.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { Grid, Typography, Box, Button } from "@mui/material";
+import { ComponentContainerCard, PageBreadcrumb } from "@src/components";
+
+const UserManagement = () => {
+  return (
+    <>
+      <Typography variant="h6" gutterBottom>
+        User Management
+      </Typography>
+    </>
+  );
+};
+
+export default UserManagement;
