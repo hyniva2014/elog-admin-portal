e8e813b70994313f2ec3bb22c6f8ad534172048f
Kolli-Bhargav <Bhargavram.kolli@hyniva.com>
2026-05-15 15:58:47 +0530

    EL-841: Resolved console errors

--- src/main.jsx
+++ src/main.jsx
@@ -38,7 +38,12 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
     <StrictMode>
       <HelmetProvider>
-        <BrowserRouter>
+        <BrowserRouter
+          future={{
+            v7_startTransition: true,
+            v7_relativeSplatPath: true,
+          }}
+        >
           <AuthProvider>
             <LayoutProvider>
               <SnackbarProvider>
