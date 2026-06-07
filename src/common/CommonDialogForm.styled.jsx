7e9ba19654196997cfbbe85004ebbc776a05a96a
Naveen-3311 <naveen@hyniva.com>
2026-06-07 20:17:59 +0530

    EL-994 : Removed Boder line

--- src/common/CommonDialogForm.styled.jsx
+++ src/common/CommonDialogForm.styled.jsx
@@ -86,7 +86,7 @@ export const BUTTON_LABELS = {
 export const DialogPaperSx = (theme) => ({
   background: "rgba(255, 255, 255, 0.78)",
   backdropFilter: "blur(4px)",
-  border: `1.5px solid ${theme.palette.primary.main}`,
+  // border: `1.5px solid ${theme.palette.primary.main}`,
   borderRadius: "14px",
   boxShadow: theme.shadows[1],
 });
