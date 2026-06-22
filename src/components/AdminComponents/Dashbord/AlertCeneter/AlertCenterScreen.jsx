97bda5f21e1219fa3eb6b82e09bdb1e94b1b31d3
harish-hyniva <harish.chaluvadi@hyniva.com>
2026-06-22 19:26:23 +0530

    EL-820ne : Implemented API integration for Alert Center module and chat history

--- src/components/AdminComponents/Dashbord/AlertCeneter/AlertCenterScreen.jsx
+++ src/components/AdminComponents/Dashbord/AlertCeneter/AlertCenterScreen.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { PageContainer } from "../../component.styled";
 import AlertCenterScreenHeader from "./AlertCenterScreenHeader";
 import AlertCenterScreenCards from "./AlertCenterScreenCards";
@@ -7,8 +7,12 @@ import {
   alertCategoryOptions,
   alertSeverityOptions,
 } from "../AdminConstant";
+import { useServices } from "../../../../services/services";
 
 const AlertCenterScreen = () => {
+  const [alerts, setAlerts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const { fetchApi } = useServices();
   const [data, setData] = useState({
     search: "",
     fromDate: null,
@@ -19,6 +23,38 @@ const AlertCenterScreen = () => {
     pageSize: 10,
   });
 
+  const formatDate = (date) => {
+    if (!date) return "";
+
+    const d = new Date(date);
+
+    return `${d.getFullYear()}-${String(d.getMonth() + 1).padStart(
+      2,
+      "0",
+    )}-${String(d.getDate()).padStart(2, "0")}`;
+  };
+
+  const fetchAlerts = async () => {
+    setLoading(true);
+
+    const endUrl =
+      `/masteradmin/alert-center/notifications` +
+      `?search=${data.search || ""}` +
+      `&from_date=${formatDate(data.fromDate)}` +
+      `&to_date=${formatDate(data.toDate)}` 
+
+    const response = await fetchApi(endUrl);
+
+    if (response?.statusCode === 200) {
+      setAlerts(response?.body?.data || []);
+    }
+
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchAlerts();
+  }, [data.search, data.fromDate, data.toDate, data.category, data.severity]);
+
   return (
     <PageContainer>
       <AlertCenterScreenHeader
@@ -28,7 +64,7 @@ const AlertCenterScreen = () => {
         categoryOptions={alertCategoryOptions}
         severityOptions={alertSeverityOptions}
       />
-      <AlertCenterScreenCards alerts={alertCenterAlerts} />
+      <AlertCenterScreenCards alerts={alerts} />
     </PageContainer>
   );
 };
