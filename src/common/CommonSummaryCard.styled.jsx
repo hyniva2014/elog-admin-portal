bc161c25224ee48fb0c63c37295f6ad9fdbe26d8
Dilip-Hyniva <dilipkumar.jaiswal@hyniva.com>
2026-06-23 15:42:27 +0530

    EL-1171:changes the height

--- src/common/CommonSummaryCard.styled.jsx
+++ src/common/CommonSummaryCard.styled.jsx
@@ -6,7 +6,7 @@ export const SummaryCardRoot = styled(Box)(({ theme }) => {
   return {
     flex: "1 1 200px",
     minWidth: 200,
-    height: 76,
+    minHeight: 76,
 
     backgroundColor: isDark
       ? theme.palette.grey[100]
