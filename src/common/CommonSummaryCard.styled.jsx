90eb73b5121137435a30075fca530a087ad6a9ab
Preethi10-25 <Preethi.kotagiri@hyniva.com>
2026-07-02 11:36:26 +0530

    EL-1257:Fixed the Count Placed Issue in SuperAdmin dashboard

--- src/common/CommonSummaryCard.styled.jsx
+++ src/common/CommonSummaryCard.styled.jsx
@@ -58,7 +58,7 @@ export const ValueText = styled(Typography, {
   color: isdashboard
     ? theme.palette.text.primary
     : theme.palette.text.secondary,
-  paddingLeft: isdashboard ? theme.spacing(6) : 0,
+  paddingLeft: 0,
 }));
 
 export const CardContentWrapper = styled(Box)(({ theme }) => ({
